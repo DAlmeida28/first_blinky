4edc0c69a16f874444f14c8a38ebeeaf0081404a
Daniel Almeida <almeida.danny@gmail.com>
2025-09-10 19:32:51 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use nrf52833_hal::{gpio, pac};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = pac::Peripherals::take().unwrap();
+    let p0 = gpio::P0::Parts::new(peripherals.p0);
+    let _row1 = p0.p0_21.into_push_pull_output(gpio::Level::High);
+    let _col1 = p0.p0_28.into_push_pull_output(gpio::Level::Low);
+
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
