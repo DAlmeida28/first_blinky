121d0ffc1fbf7e1af174ffad790d4fbc11b87808
Daniel Almeida <almeida.danny@gmail.com>
2025-09-11 20:03:35 -0500

    updated dev environment

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
+use embedded_hal::digital::OutputPin;
 use nrf52833_hal::{gpio, pac};
+use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
     let peripherals = pac::Peripherals::take().unwrap();
-    let p0 = gpio::P0::Parts::new(peripherals.p0);
-    let _row1 = p0.p0_21.into_push_pull_output(gpio::Level::High);
+    let p0 = gpio::p0::Parts::new(peripherals.P0);
+    let mut row1 = p0.p0_21.into_push_pull_output(gpio::Level::Low);
+    let mut row2 = p0.p0_22.into_push_pull_output(gpio::Level::Low);
     let _col1 = p0.p0_28.into_push_pull_output(gpio::Level::Low);
 
     #[allow(clippy::empty_loop)]
-    loop {}
+    loop {
+        row1.set_high().unwrap();
+        row1.set_low().unwrap();
+        row2.set_high().unwrap();
+        row2.set_low().unwrap();
+    }
 }
