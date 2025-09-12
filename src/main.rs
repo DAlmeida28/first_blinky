900088b30cd7c2bbb91fd21170fa6777ab1de364
Daniel Almeida <almeida.danny@gmail.com>
2025-09-11 20:41:24 -0500

    Updated code to blink each led left to right top to bottom

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,63 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use embedded_hal::digital::OutputPin;
-use nrf52833_hal::{gpio, pac};
-use panic_halt as _;
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+
+#[rustfmt::skip]
+const PIXELS: [(usize, usize); 25] = [
+    (0, 0),
+    (0, 1),
+    (0, 2),
+    (0, 3),
+    (0, 4),
+    (1, 0),
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (2, 0),
+    (2, 1),
+    (2, 2),
+    (2, 3),
+    (2, 4),
+    (3, 0),
+    (3, 1),
+    (3, 2),
+    (3, 3),
+    (3, 4),
+    (4, 0),
+    (4, 1),
+    (4, 2),
+    (4, 3),
+    (4, 4)
+];
 
 #[entry]
 fn main() -> ! {
-    let peripherals = pac::Peripherals::take().unwrap();
-    let p0 = gpio::p0::Parts::new(peripherals.P0);
-    let mut row1 = p0.p0_21.into_push_pull_output(gpio::Level::Low);
-    let mut row2 = p0.p0_22.into_push_pull_output(gpio::Level::Low);
-    let _col1 = p0.p0_28.into_push_pull_output(gpio::Level::Low);
+    rtt_init_print!();
 
-    #[allow(clippy::empty_loop)]
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    #[rustfmt::skip]
+    let mut leds = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let mut last_led = (0, 0);
+
     loop {
-        row1.set_high().unwrap();
-        row1.set_low().unwrap();
-        row2.set_high().unwrap();
-        row2.set_low().unwrap();
+        for current_led in PIXELS.iter() {
+            leds[last_led.0][last_led.1] = 0;
+            leds[current_led.0][current_led.1] = 1;
+            display.show(&mut timer, leds, 100);
+            last_led = *current_led;
+        }
     }
 }
